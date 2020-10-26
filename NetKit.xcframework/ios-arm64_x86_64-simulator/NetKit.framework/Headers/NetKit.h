891c4103e683396cea19f115fcb8f5e73850d274
Aline Ordoñez <aline.ordonez@cbsinteractive.com>
2020-10-25 23:36:47 -0600

    Added XCFramework

---
+++ NetKit.xcframework/ios-arm64_x86_64-simulator/NetKit.framework/Headers/NetKit.h
@@ -0,0 +1,18 @@
+//
+//  NetKit.h
+//  NetKit
+//
+//  Created by Aline Arely Ordonez Garcia on 25/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetKit.
+FOUNDATION_EXPORT double NetKitVersionNumber;
+
+//! Project version string for NetKit.
+FOUNDATION_EXPORT const unsigned char NetKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetKit/PublicHeader.h>
+
+
